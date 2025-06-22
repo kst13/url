59fad4d0726de80440597cb02d82601ac1b10426
kst13 <dkgell@naver.com>
2025-06-22 18:49:16 +0900

    refactor(id generator) : generator 개선

--- src/main/java/kr/co/kst/url/application/ShortUrlCommandService.java
+++ src/main/java/kr/co/kst/url/application/ShortUrlCommandService.java
@@ -24,6 +24,7 @@ public class ShortUrlCommandService {
     public ShortUrlResponse create(String url) {
 
         ShortUrl shortUrl = ShortUrl.create(idGenerator, url);
+        ㄴ어린ㅇ
 
         kafkaTemplate.send("short-url-topic", new ShortUrlMessage(shortUrl.getCode(), url));
         String result = DOMAIN + "/" + shortUrl.getCode();
